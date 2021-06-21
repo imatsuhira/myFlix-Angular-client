18ba8cd0726bff9574ca10f680538f5927a169b8
imatsuhira <hello.melancholinista@gmail.com>
2021-06-21 15:34:07 +0200

    added genre and Director, favoritebutton to moviecard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-  // This is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent,
-      {
-        // Assigning the dialog a width
-        width: '280px'
-      });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog width
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon'
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -15,14 +17,21 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent},
+  { path: 'movies', component: MovieCardComponent},
+  { path: '', redirectTo: 'welcome', pathMatch:'prefix'},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +44,9 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
