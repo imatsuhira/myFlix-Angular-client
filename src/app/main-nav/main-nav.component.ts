178fd9d8a6f3b33d8624eb3469339e8a017c5f95
imatsuhira <hello.melancholinista@gmail.com>
2021-06-23 17:33:26 +0200

    logoutUser function implemented

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -10,12 +9,16 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  constructor(
+    public router: Router,
+    public snackBar: MatSnackBar
+  ) { }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  logoutUser(){
+    localStorage.clear()
+    this.snackBar.open('user logout successfully', 'OK', {
+      duration: 2000
+    })
+    this.router.navigate(['welcome'])
+  }
 }
